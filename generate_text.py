59031239e0162abeb2cfe2eaceec17fdd62bda79
KOSASIH <kosasihg88@gmail.com>
2023-11-05 14:11:21 +0700

    Create generate_text.py

---
+++ generate_text.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, LSTM, Dense
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# Define the input sequence length and vocabulary size
+input_seq_length = 100
+vocab_size = 10000
+
+# Define the LSTM model architecture
+input_layer = Input(shape=(input_seq_length,))
+embedding_layer = tf.keras.layers.Embedding(vocab_size, 256)(input_layer)
+lstm_layer = LSTM(256)(embedding_layer)
+output_layer = Dense(vocab_size, activation='softmax')(lstm_layer)
+
+# Create the model
+model = Model(inputs=input_layer, outputs=output_layer)
+model.compile(optimizer='adam', loss='categorical_crossentropy')
+
+# Train the model on your dataset
+model.fit(x_train, y_train, batch_size=32, epochs=10)
+
+# Generate text based on given prompts
+def generate_text(prompt):
+    # Tokenize the prompt
+    prompt_tokens = tokenizer.texts_to_sequences([prompt])
+    prompt_tokens = pad_sequences(prompt_tokens, maxlen=input_seq_length)
+
+    # Generate text using the trained model
+    generated_tokens = model.predict(prompt_tokens)
+    generated_text = tokenizer.sequences_to_texts(generated_tokens)[0]
+
+    return generated_text
+
+prompt = "As a leading AI company in the healthcare industry, we strive to improve patient care through innovative solutions."
+generated_text = generate_text(prompt)
+print(generated_text)
